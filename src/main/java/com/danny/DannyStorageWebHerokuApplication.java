a16b8ef6708dd299a342cff7d2f263fd03c5fb55
dannythreeka <dannythreekai@gmail.com>
2016-10-15 15:17:10 +0900

    Thrid commit

--- src/main/java/com/danny/DannyStorageWebHerokuApplication.java
+++ src/main/java/com/danny/DannyStorageWebHerokuApplication.java
@@ -2,11 +2,13 @@ package com.danny;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class DannyStorageWebHerokuApplication {
+public class DannyStorageWebHerokuApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DannyStorageWebHerokuApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DannyStorageWebHerokuApplication.class, args);
+  }
 }
